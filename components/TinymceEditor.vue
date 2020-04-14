2eabc7f3318c0181d8500965401195222ab17573
lebai <liu@lebaideMacBook-Pro.local>
2020-04-14 15:54:15 +0800

    add image video for mce

--- components/TinymceEditor.vue
+++ components/TinymceEditor.vue
@@ -28,7 +28,7 @@
         plugins: 'print preview paste importcss searchreplace autolink autosave save directionality code visualblocks visualchars fullscreen image link media template codesample table charmap hr pagebreak nonbreaking anchor toc insertdatetime advlist lists wordcount imagetools textpattern noneditable help charmap quickbars emoticons',
         imagetools_cors_hosts: ['picsum.photos'],
         menubar: 'file edit view insert format tools table help',
-        toolbar: 'undo redo | bold italic underline strikethrough | fontselect fontsizeselect formatselect | alignleft aligncenter alignright alignjustify | outdent indent |  numlist bullist | forecolor backcolor removeformat | pagebreak | charmap emoticons | fullscreen  preview save print | insertfile image media template link anchor codesample | ltr rtl',
+        toolbar: 'undo redo | bold italic underline strikethrough | fontselect fontsizeselect formatselect | alignleft aligncenter alignright alignjustify | outdent indent |  numlist bullist | forecolor backcolor removeformat | pagebreak | superscript subscript charmap emoticons | fullscreen  preview save print | insertfile image media template link anchor codesample | ltr rtl',
         toolbar_sticky: true,
         codesample_global_prismjs: true,
         autosave_ask_before_unload: true,
@@ -52,20 +52,38 @@
         ],
         importcss_append: true,
         height: 400,
+        // images_upload_url: 'http://localhost:8080/api/',
+        images_upload_handler: function (blobInfo, success, failure) {
+          console.log('image upload...')
+          success(blobInfo.filename())
+          // success('http://www.baidu.com/img/bd_logo1.png?qua=high')
+        },
+        file_picker_types: 'media',
         file_picker_callback: function (callback, value, meta) {
-          /* Provide file and text for the link dialog */
-          if (meta.filetype === 'file') {
-            callback('https://www.google.com/logos/google.jpg', { text: 'My text' });
-          }
+          let input = document.createElement('input');
+          input.setAttribute('type', 'file');
+          // 你可以给input加accept属性来限制上传的文件类型
+          // input.setAttribute('accept', '.jpg,.png');
+          input.click();
+          input.onchange = function(){
+            let file = this.files[0];
+            let formData = new FormData();
+            formData.append('file', file, file.name);
+            callback(file.name, { source2: 'alt.ogg', poster: 'https://www.google.com/logos/google.jpg' });
+            // /* Provide file and text for the link dialog */
+            // if (meta.filetype === 'file') {
+            //   callback('https://www.google.com/logos/google.jpg', { text: 'My text' });
+            // }
 
-          /* Provide image and alt text for the image dialog */
-          if (meta.filetype === 'image') {
-            callback('https://www.google.com/logos/google.jpg', { alt: 'My alt text' });
-          }
+            // /* Provide image and alt text for the image dialog */
+            // if (meta.filetype === 'image') {
+            //   callback('https://www.google.com/logos/google.jpg', { alt: 'My alt text' });
+            // }
 
-          /* Provide alternative source and posted for the media dialog */
-          if (meta.filetype === 'media') {
-            callback('movie.mp4', { source2: 'alt.ogg', poster: 'https://www.google.com/logos/google.jpg' });
+            // /* Provide alternative source and posted for the media dialog */
+            // if (meta.filetype === 'media') {
+            //   callback('movie.mp4', { source2: 'alt.ogg', poster: 'https://www.google.com/logos/google.jpg' });
+            // }
           }
         },
         templates: [
@@ -78,7 +96,7 @@
         image_caption: true,
         quickbars_selection_toolbar: 'bold italic | quicklink h2 h3 blockquote quickimage quicktable',
         noneditable_noneditable_class: "mceNonEditable",
-        toolbar_mode: 'sliding',
+        toolbar_mode: 'wrap',
         contextmenu: "link image imagetools table",
 
       }
